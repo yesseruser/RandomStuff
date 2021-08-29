840b5487396e9d9cb7563c3b07d59ad86ddbb5de
yesseruser <yesseruseryt@gmail.com>
2021-08-29 19:52:39 +0200

    Cleaned up a little more

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,11 +6,11 @@ namespace Test
     {
         private static void Main(string[] args)
         {
-            Person testGuy = new Person("Doesn'tknow Hisname", new Gender(true));
+            Person testGuy = new("Doesn'tknow Hisname", new Gender(true));
             testGuy.PrintInfo();
             testGuy.TimePass(30);
             testGuy.PrintInfo();
-            Consumable SuperFood = new Consumable(true, "Super Food", 100, 100, 100);
+            Consumable SuperFood = new(true, "Super Food", 100, 100, 100);
             testGuy.Consume(SuperFood, true);
             testGuy.PrintInfo();
         }
